141dda612dfdc0d5d361c465b736102984707c01
Max Popkin <mpopkin@proactivemgmt.com>
2024-04-30 10:44:44 -0400

    Initial commit with AzureFunction and html form

---
+++ AzureFunction/WebPageFunction/run.ps1
@@ -0,0 +1,13 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+$html = get-content "$psscriptroot\form.html" -raw
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK 
+    ContentType = "text/html"
+    Body = $html
+})
