0d78f81acca6511d20ebbce49887d70a0f85bc6e
Max Popkin <mpopkin@proactivemgmt.com>
2024-05-08 09:26:48 -0400

    added htmx elements to form and post function to powershell script

--- AzureFunction/WebPageFunction/run.ps1
+++ AzureFunction/WebPageFunction/run.ps1
@@ -1,13 +1,42 @@
-using namespace System.Net
-
-# Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-$html = get-content "$psscriptroot\form.html" -raw
+function LoadHtmlForm {
+    $htmlPath =  "$PSScriptRoot\form.html"
+    $htmlContent = Get-Content $htmlPath -Raw
+    return $htmlContent
+}
 
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK 
-    ContentType = "text/html"
-    Body = $html
-})
+function ProcessFormData {
+    param($formData)
+    $parsedData = [System.Web.HttpUtility]::ParseQueryString($formData)
+    foreach ($key in $parsedData.AllKeys) {
+        Write-Output "$key = $($parsedData[$key])"
+    }
+}
+
+$method = $Request.Method
+
+if ($method -eq 'GET') {
+    $responseBody = LoadHtmlForm
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [System.Net.HttpStatusCode]::OK
+        ContentType = "text/html"
+        Body = $responseBody
+    })
+} elseif ($method -eq 'POST') {
+    if ($Request.Headers.'Content-Type' -contains 'application/x-www-form-urlencoded') {
+        $formData = [System.Text.Encoding]::UTF8.GetString($Request.Body)
+        ProcessFormData -formData $formData
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [System.Net.HttpStatusCode]::OK
+            ContentType = "text/plain"
+            Body = "Form data processed successfully"
+        })
+    } else {
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [System.Net.HttpStatusCode]::UnsupportedMediaType
+            ContentType = "text/plain"
+            Body = "Unsupported Media Type"
+        })
+    }
+}
