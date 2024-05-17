2152714b81b849a4f8a9feca8325b4bdc120bdc1
Marcelo Escobar <mescobar@proactivemgmt.com>
2024-05-17 15:19:44 -0300

    Included Id

--- AzureFunction/serve/run.ps1
+++ AzureFunction/serve/run.ps1
@@ -1,10 +1,19 @@
 param($Request, $TriggerMetadata)
 
-$htmlPath    = "$PSScriptRoot\form.html"
+# Retrieve the id parameter from the request
+$id = $Request.Query.id
+
+$htmlPath = "$PSScriptRoot\form.html"
 $htmlContent = Get-Content $htmlPath -Raw
 
+ 
+# Inject the hidden field with the GUID into the HTML content
+$htmlContent = $htmlContent -replace '</form>', "<input type='hidden' name='id' value='$id' /></form>"
+
+
+
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [System.Net.HttpStatusCode]::OK
-    ContentType = "text/html"
-    Body = $htmlContent
-})
+        StatusCode  = [System.Net.HttpStatusCode]::OK
+        ContentType = "text/html"
+        Body        = $htmlContent
+    })
--- AzureFunction/submit/run.ps1
+++ AzureFunction/submit/run.ps1
@@ -2,12 +2,19 @@ param($Request, $TriggerMetadata)
 
 $Parameters = $Request.Query
 
-foreach ($key in $Parameters.Keys) {
-    Write-Host "$key = $($Parameters[$key])"
-}
+# foreach ($key in $Parameters.Keys) {
+#     Write-Host "$key = $($Parameters[$key])"
+# }
+$paramHashtable = @{}
 
+# Populate the hashtable with the query parameters
+foreach ($key in $Parameters.Keys) {
+    $paramHashtable[$key] = $Parameters[$key]
+}
+$jsonResponse = $paramHashtable | ConvertTo-Json
+Write-Host  $jsonResponse
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [System.Net.HttpStatusCode]::OK
-    ContentType = "text/plain"
-    Body = "Form data processed successfully"
-})
+        StatusCode  = [System.Net.HttpStatusCode]::OK
+        ContentType = "application/json"#"text/plain"
+        Body        = $jsonResponse #"Form data processed successfully"  + 
+    })
