1d52881b12e955cd697f6e290f52eb03fc10757c
Marcelo Escobar <mescobar@proactivemgmt.com>
2024-05-21 12:16:57 -0300

    Form quesion renamed . Removed harcoded values.

--- AzureFunction/submit/run.ps1
+++ AzureFunction/submit/run.ps1
@@ -1,5 +1,8 @@
-param($Request, $TriggerMetadata)
 
+param($Request, $TriggerMetadata)
+# . C:\Repos\cg-hopescalesurvey\cg-hopescalesurvey\AzureFunction\submit\UpdateTableRow.ps1
+# . .submit\UpdateTableRow.ps1
+. .\submit\UpdateTableRow.ps1
 $Parameters = $Request.Query
 
 # foreach ($key in $Parameters.Keys) {
@@ -11,18 +14,20 @@ $paramHashtable = @{}
 foreach ($key in $Parameters.Keys) {
     $paramHashtable[$key] = $Parameters[$key]
 }
+
+$RowKey = $Parameters["id"]
+$PartitionKey = $RowKey[-1]
+$paramHashtable.Remove("id") 
+
 # Define the parameters
-$PartitionKey = '2'
-$RowKey = '1234123412'
-$PropertiesToUpdate = @{
-    'Field1' = 'Updated From the form 1 '
-    'Field2' = 'Updated From the form 12'
-}
+# $PartitionKey = '2'
+# $RowKey = '1234123412'
 # Call the function
 
-. C:\Repos\cg-hopescalesurvey\cg-hopescalesurvey\AzureFunction\submit\UpdateTableRow.ps1
 
-Update-TableRow -PartitionKey $PartitionKey -RowKey $RowKey -PropertiesToUpdate $PropertiesToUpdate
+
+Write-Host  $paramHashtable
+Update-TableRow -PartitionKey $PartitionKey -RowKey $RowKey -PropertiesToUpdate $paramHashtable
 Write-Host "Update completed successfully."
 
 $jsonResponse = $paramHashtable | ConvertTo-Json
@@ -30,6 +35,6 @@ Write-Host  $jsonResponse
 
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode  = [System.Net.HttpStatusCode]::OK
-        ContentType = "application/json"#"text/plain"
-        Body        = $jsonResponse #"Form data processed successfully"  + 
+        ContentType = "text/plain"
+        Body        = "Form data processed successfully. Thanks you!"  
     })
