1e604bb14bf421e1b41c1547d745ff1d3c7ace12
Joseph Robertson <jrobertson@proactivemgmt.com>
2024-08-29 00:59:03 -0400

    Remove pwsh scripts

--- ContainerApps/practicefusion/get-logs.ps1
+++
@@ -1,8 +0,0 @@
-#!/bin/pwsh
-
-$SUBSCRIPTION_ID    = '95257f24-41c8-4dc2-8657-18f74be4cfcb'
-$IMAGE              = 'practicefusion'
-$RESOURE_GROUP_NAME = 'HopeScaleSurvey'
-
-az account set --subscription $SUBSCRIPTION_ID &&
-az containerapp logs show --name $IMAGE --container $IMAGE --resource-group $RESOURE_GROUP_NAME
--- ContainerApps/practicefusion/publish-image.ps1
+++
@@ -1,8 +0,0 @@
-#!/bin/pwsh
-
-$IMAGE    = 'practicefusion'
-$ACR_NAME = 'commongroundcr.azurecr.io'
-
-docker login $ACR_NAME &&
-docker build -t "$ACR_NAME/$IMAGE" . &&
-docker push "$ACR_NAME/$IMAGE" 
