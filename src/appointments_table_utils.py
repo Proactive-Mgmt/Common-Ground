68376561b81fcc067b055ddc3c50ca318c0d61aa
Joey Robertson <jrobertson@proactivemgmt.com>
2025-05-01 15:53:54 -0400

    Split out individual create_new_appointment()

--- src/appointments_table_utils.py
+++ src/appointments_table_utils.py
@@ -17,39 +17,55 @@ def calculate_row_key(patient_dob: date, patient_name: str, patient_phone: str, 
     row_key = str(uuid.UUID(md5_hash))
     return row_key
 
-def save_appointments(appointments: list[PracticeFusionAppointment]):
-    logger = ptmlog.get_logger()
-
+def create_new_appointment(
+    patient_name      : str,
+    patient_dob       : date,
+    patient_phone     : str,
+    appointment_time  : datetime,
+    appointment_status: str,
+    provider          : str,
+    type              : str,
+) -> None: 
+    """
+    Create a new appointment entity in the Azure Table Storage.
+    Raises azure.core.exceptions.ResourceExistsError if the entity already exists.
+    """
     STORAGE_ACCOUNT_CONNECTION_STRING = os.environ['STORAGE_ACCOUNT_CONNECTION_STRING']
     table_client = TableClient.from_connection_string(STORAGE_ACCOUNT_CONNECTION_STRING, 'appointments')
 
-    for appointment in appointments:
-        # Generate row_key by hashing compound key (dob+lastname+phonenumber)
-        row_key = calculate_row_key(
-            patient_dob      = appointment.patient_dob,
-            patient_name     = appointment.patient_name,
-            patient_phone    = appointment.patient_phone,
-            appointment_time = appointment.appointment_time,
-        )
+    row_key = calculate_row_key(patient_dob, patient_name, patient_phone, appointment_time)
 
-        table_entity = TableEntity(
-            RowKey            = row_key,
-            PartitionKey      = row_key[-1],
-            sentOn            = '',
-            message_sid       = '',
-            patientName       = appointment.patient_name,
-            patientDOB        = appointment.patient_dob.strftime(r'%Y-%m-%d'),
-            patientPhone      = appointment.patient_phone,
-            appointmentTime   = appointment.appointment_time.strftime(r'%Y-%m-%dT%H:%M'),
-            appointmentStatus = appointment.appointment_status,
-            provider          = appointment.provider,
-            type              = appointment.type,
-        )
+    table_client.create_entity(TableEntity(
+        RowKey            = row_key,
+        PartitionKey      = row_key[-1],
+        sentOn            = '',
+        message_sid       = '',
+        patientName       = patient_name,
+        patientDOB        = patient_dob.strftime(r'%Y-%m-%d'),
+        patientPhone      = patient_phone,
+        appointmentTime   = appointment_time.strftime(r'%Y-%m-%dT%H:%M'),
+        appointmentStatus = appointment_status,
+        provider          = provider,
+        type              = type,
+    ))
+
+def save_appointments(appointments: list[PracticeFusionAppointment]):
+    logger = ptmlog.get_logger()
+
+    for appointment in appointments:
         try:
-            logger.info('creating table_entity', table_entity=table_entity)
-            table_client.create_entity(table_entity)
+            logger.info('creating table_entity', appointment=appointment)
+            create_new_appointment(
+                patient_name      = appointment.patient_name,
+                patient_dob       = appointment.patient_dob,
+                patient_phone     = appointment.patient_phone,
+                appointment_time  = appointment.appointment_time,
+                appointment_status= appointment.appointment_status,
+                provider          = appointment.provider,
+                type              = appointment.type,
+            )
         except ResourceExistsError:
-            logger.warning('table_entity already exists', table_entity=table_entity)
+            logger.warning('table_entity already exists', appointment=appointment)
 
 def get_appointments() -> list[TableAppointment]:
     STORAGE_ACCOUNT_CONNECTION_STRING = os.environ['STORAGE_ACCOUNT_CONNECTION_STRING']
